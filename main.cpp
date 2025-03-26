ca2badbc29d9e481150c48f85ef91e52890a2675
b010522m <b010522m@student.staffs.ac.uk>
2025-03-26 08:44:15 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 /*
     This version of the SFML "hello world" is statically linked, you may wish to try the dynamically linked version as well.
 */
 int WinMain()
 {
-    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "SFML works!");
 
     while (window.isOpen())
     {
+        window.setFramerateLimit(60);
         // Event polling section of code - this must be done in the thread which created the window
         // we will talk about threading later, but essentially this must be done here
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
-        }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+            window.clear();
+            window.display();
+        }
     }
 }
